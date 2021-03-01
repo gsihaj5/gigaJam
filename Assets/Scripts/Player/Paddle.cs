5d446909a5ab7512e80efb8faff5dcd7b6e653e2
Gerry Sihaj <gsihaj5@gmail.com>
2021-03-02 00:40:32 +0700

    movement

---
+++ Assets/Scripts/Player/Paddle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] private GameObject player1;
+
+    [SerializeField] private GameObject player2;
+
+    private Transform _transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandlePosition();
+        HandleRotation();
+        HandleWidth();
+    }
+
+    private void HandlePosition()
+    {
+        Vector3 player1Pos = player1.transform.position;
+        Vector3 player2Pos = player2.transform.position;
+
+        float xPos = (player1Pos.x + player2Pos.x) / 2;
+        float yPos = (player1Pos.y + player2Pos.y) / 2;
+        _transform.position = new Vector3(xPos, yPos, 0);
+    }
+
+    private void HandleRotation()
+    {
+        Vector3 player1Pos = player1.transform.position;
+        Vector3 player2Pos = player2.transform.position;
+
+        double angle = Math.Atan((player2Pos.x - player1Pos.x) / (player1Pos.y - player2Pos.y)) * (180 / Math.PI);
+
+        _transform.eulerAngles = new Vector3(0, 0, (float) angle - 90);
+    }
+
+    private void HandleWidth()
+    {
+        Vector3 player1Pos = player1.transform.position;
+        Vector3 player2Pos = player2.transform.position;
+
+        float xDif = player1Pos.x - player2Pos.x;
+        float yDif = player1Pos.y - player2Pos.y;
+
+        double width = Math.Sqrt(xDif * xDif + yDif * yDif) * 100;
+
+        _transform.transform.localScale = new Vector3((float) width, 20, 0);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private int player;
+        [SerializeField] private float speed;
+
+        private Rigidbody2D rb;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            rb.velocity = new Vector2(
+                Input.GetAxisRaw($"HorizontalPlayer{player}") * speed,
+                Input.GetAxisRaw($"VerticalPlayer{player}") * speed
+            );
+        }
+    }
+}
