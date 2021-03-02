0ed5466a3b41ce84e1e8e90ab39f465f6a4021fb
Gerry Sihaj <gsihaj5@gmail.com>
2021-03-02 16:23:58 +0700

    small changes

---
+++ Assets/Scripts/MainMenu/ButtonHandler.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace MainMenu
+{
+    public class ButtonHandler : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+    {
+        [SerializeField] private Sprite idle;
+        [SerializeField] private Sprite hover;
+        [SerializeField] private Sprite clicked;
+
+        private Image _image;
+
+        private void Start()
+        {
+            _image = GetComponent<Image>();
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            _image.sprite = hover;
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            _image.sprite = clicked;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            _image.sprite = idle;
+        }
+    }
+}
