1924f23e02778a11ad1ae5154a766148b6647d67
Gerry Sihaj <gsihaj5@gmail.com>
2021-03-02 17:24:22 +0700

    small changes

---
+++ Assets/Scripts/MainMenu/Routing.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MainMenu
+{
+    public class Routing : MonoBehaviour
+    {
+        public void PlayGame()
+        {
+            SceneManager.LoadScene("Level1");
+        }
+
+        public void ExitGame()
+        {
+        }
+    }
+}
